1c9f89944bb8e580850aa65582dfa843f6f1c823
AtulPant2704 <90546508+AtulPant2704@users.noreply.github.com>
2021-10-05 20:50:24 +0530

    added description of books

--- src/App.js
+++ src/App.js
@@ -3,37 +3,52 @@ import "./styles.css";
 
 const bookDB = {
   javascript: [
-    { name: "Eloquent JavaScript", rating: "4/5" },
-    { name: "You Don't Know JS", rating: "3.5/5" },
-    { name: "A Smarter Way To Learn Javascript", rating: "3/5"}
+    { name: "Eloquent JavaScript", 
+      rating: "4/5",
+      description: "This is a book about JavaScript, programming, and the wonders of the digital"
+    },
+    { name: "You Don't Know JS", 
+      rating: "3.5/5",
+      description: "The You Don’t Know JS is a series of books which explores JavaScript types in greater depth"
+    },
+    { name: "A Smarter Way To Learn Javascript", 
+      rating: "3/5",
+      description: "Books specially designed for freshers"
+    }
   ],
 
   fiction: [
     {
       name: "Shiva Trilogy",
-      rating: "5/5"
+      rating: "5/5",
+      description: "This is the book on Shiva, the simple man whose karma re-cast him as our Mahadev, the God of Gods"
     },
     {
       name: "Harry Potter and the Sorcerer's Stone",
-      rating: "4.5/5"
+      rating: "4.5/5",
+      description: "The trio find a newspaper report about an attempted robbery of a Gringotts vault—one that Hagrid had visited when helping Harry"
     },
     {
       name: "The Alchemist",
-      rating: "4/5"
+      rating: "4/5",
+      description: " This story is about an Andalusian shepherd boy named Santiago, who travels from his homeland in Spain to the Egyptian desert in search of a treasure"
     }
   ],
   business: [
     {
       name: "Never Split the Difference",
-      rating: "3.5/5"
+      rating: "3.5/5",
+      description: "It is a riveting, indispensable handbook of negotiation principles culled and perfected"
     },
     {
       name: "Loonshots",
-      rating: "5/5"
+      rating: "5/5",
+      description: "Loonshots is the first to apply this science to the spread of breakthrough ideas"
     },
     {
       name: "Critical Chain",
-      rating: "4.5/5"
+      rating: "4.5/5",
+      description: "The Critical Chain is the longest path in the schedule, and considers activity interdependence and resource constraints"
     }
   ]
 };
@@ -85,7 +100,8 @@ export default function App() {
               }}
             >
               <div style={{ fontSize: "larger" }}>{book.name}</div>
-              <div style={{ fontSize: "smaller" }}>{book.rating}</div>
+              <div style={{ fontSize: "smaller" }}>{book.rating}</div>              
+              <div style={{ fontSize: "large" }}>{book.description}</div>
             </li>
           ))}
         </ul>
